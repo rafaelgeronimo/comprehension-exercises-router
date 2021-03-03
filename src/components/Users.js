9ef6c2fd2285982b72d94d55aa2279ed58d56c27
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-03-03 10:20:53 -0300

    Exercises 7 - 9 done

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import { useParams } from 'react-router-dom';
 
 class Users extends Component {
   render() {
+    const {
+      greetingMessage = 'Hi There',
+      match: { params: { id }
+    } } = this.props;
+
     return (
       <div>
         <h2>Users</h2>
-        <p> My awesome Users component </p>
+        <p> 
+          {`${greetingMessage} User ${id}, this is my awesome Users component`}
+        </p>
       </div>
     );
   }
 };
+
+// esse exercício 9 foi uma sacanagem, não foi ensinado dessa maneira
 
 export default Users;
